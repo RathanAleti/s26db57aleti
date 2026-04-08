33b6282d47da5108e9b4da9bd9b7c2d81e75b149
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-08 11:52:53 -0500

    Add costumes view page

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var Costume = require("./models/costume");
 var resourceRouter = require('./routes/resource');
+var costumesRouter = require('./routes/costumes');
 
 // Seed DB
 async function recreateDB(){
@@ -59,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/resource', resourceRouter);
+app.use('/costumes', costumesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
