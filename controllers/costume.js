1f32a8181f4789574af535c2b65efbda9eacdb2d
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-07 21:52:14 -0500

    Implement costume list endpoint

--- controllers/costume.js
+++ controllers/costume.js
@@ -1,10 +1,16 @@
 var Costume = require('../models/costume');
 
 // List of all Costumes
-exports.costume_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume list');
+exports.costume_list = async function(req, res) {
+    try {
+        theCostumes = await Costume.find();
+        res.send(theCostumes);
+    }
+    catch(err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
-
 // for a specific Costume
 exports.costume_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
