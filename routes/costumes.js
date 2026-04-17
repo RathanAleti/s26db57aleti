0760836f6980572a6a1deea3dd78727974748d77
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-17 10:32:22 -0500

    Part 1: Add update buttons to collection view

--- routes/costumes.js
+++ routes/costumes.js
@@ -1,19 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-// Require controller modules
 var api_controller = require('../controllers/api');
 var costume_controller = require('../controllers/costume');
 
-/// API ROUTE ///
-router.get('/', api_controller.api);
-
-/// COSTUME ROUTES ///
 router.post('/costumes', costume_controller.costume_create_post);
 router.delete('/costumes/:id', costume_controller.costume_delete);
 router.put('/costumes/:id', costume_controller.costume_update_put);
 router.get('/costumes/:id', costume_controller.costume_detail);
-router.get('/costumes', costume_controller.costume_list);
+router.get('/costumes', costume_controller.costume_view_all_Page);
 router.get('/detail', costume_controller.costume_view_detail_Page);
 router.get('/create', costume_controller.costume_view_create_Page);
 router.get('/update', costume_controller.costume_view_update_Page);
