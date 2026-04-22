bc072e856f84ca8a165d964448cb9df86de09516
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-22 10:48:53 -0500

    Part 5 model validation

--- models/costume.js
+++ models/costume.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const costumeSchema = mongoose.Schema({
     costume_type: String,
     size: String,
-    cost: Number
+    cost: { type: Number, min: [1, 'Cost must be at least 1'], max: [500, 'Cost cannot exceed 500'] },
+    
 });
 
 module.exports = mongoose.model("Costume", costumeSchema);
