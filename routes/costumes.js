061a66665fe2986260c933d7e5a1a46d1a72ead3
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-22 10:26:31 -0500

    Part 4 auth protection

--- routes/costumes.js
+++ routes/costumes.js
@@ -4,6 +4,13 @@ var router = express.Router();
 var api_controller = require('../controllers/api');
 var costume_controller = require('../controllers/costume');
 
+const secured = (req, res, next) => {
+    if (req.user){
+      return next();
+    }
+    res.redirect("/login");
+}
+
 router.post('/costumes', costume_controller.costume_create_post);
 router.delete('/costumes/:id', costume_controller.costume_delete);
 router.put('/costumes/:id', costume_controller.costume_update_put);
@@ -11,7 +18,7 @@ router.get('/costumes/:id', costume_controller.costume_detail);
 router.get('/costumes', costume_controller.costume_view_all_Page);
 router.get('/detail', costume_controller.costume_view_detail_Page);
 router.get('/create', costume_controller.costume_view_create_Page);
-router.get('/update', costume_controller.costume_view_update_Page);
+router.get('/update', secured, costume_controller.costume_view_update_Page);
 router.get('/delete', costume_controller.costume_view_delete_Page);
 
 module.exports = router;
