4495c71c3a4c7829119eb583af48e0098c2dd24f
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-22 19:17:57 -0500

    Part 6 collection buttons

--- routes/costumes.js
+++ routes/costumes.js
@@ -15,7 +15,7 @@ router.post('/costumes', costume_controller.costume_create_post);
 router.delete('/costumes/:id', costume_controller.costume_delete);
 router.put('/costumes/:id', costume_controller.costume_update_put);
 router.get('/costumes/:id', costume_controller.costume_detail);
-router.get('/costumes', costume_controller.costume_view_all_Page);
+router.get('/', costume_controller.costume_view_all_Page);
 router.get('/detail', costume_controller.costume_view_detail_Page);
 router.get('/create', costume_controller.costume_view_create_Page);
 router.get('/update', secured, costume_controller.costume_view_update_Page);
