d9c4326cb30827483b893e8eeff260047c95d1d5
RathanAleti <aletirathanreddy77@gmail.com>
2026-04-22 19:36:07 -0500

    Bonus validation and protected endpoints

--- models/costume.js
+++ models/costume.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const costumeSchema = mongoose.Schema({
-    costume_type: String,
+    costume_type: { type: String, minlength: [2, 'Costume type must be at least 2 characters'], maxlength: [20, 'Costume type cannot exceed 20 characters'] },
     size: String,
     cost: { type: Number, min: [1, 'Cost must be at least 1'], max: [500, 'Cost cannot exceed 500'] },
     
--- routes/costumes.js
+++ routes/costumes.js
@@ -17,8 +17,9 @@ router.put('/costumes/:id', costume_controller.costume_update_put);
 router.get('/costumes/:id', costume_controller.costume_detail);
 router.get('/', costume_controller.costume_view_all_Page);
 router.get('/detail', costume_controller.costume_view_detail_Page);
-router.get('/create', costume_controller.costume_view_create_Page);
+router.get('/create', secured, costume_controller.costume_view_create_Page);
 router.get('/update', secured, costume_controller.costume_view_update_Page);
-router.get('/delete', costume_controller.costume_view_delete_Page);
+router.get('/delete', secured, costume_controller.costume_view_delete_Page);
+
 
 module.exports = router;
